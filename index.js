14900c053b0ce6655a55c5442d70b28fdf707dcc
nvize <confuzzulation0@gmail.com>
2022-01-05 22:49:05 -0600

    added ability for bot to choose random problems from directory

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const config = require('./config.json');
+const fs = require('fs');
+const akochanProblems = fs.readdirSync('./akochans');
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -11,7 +13,7 @@ client.on("messageCreate", async (message) => {
     if (message.author.bot) return false; 
     if (message.content.toLowerCase() == "!akochan") {
         let user = message.author;
-        let game = require("./akochans/akochan-1.json");
+        let game = require(`./akochans/${akochanProblems[Math.floor(Math.random() * akochanProblems.length)]}`);
         message.reply(`Puzzle name: ${game.name}\n`
             + `You are ${game.seat}.\n`
             + `East discards: ${game.eastDiscards}, East calls: ${game.eastCalls}\n`
@@ -34,6 +36,8 @@ client.on("messageCreate", async (message) => {
             if (ranking > -1) {
                 if (ranking === 0) {
                     responseIntro = responseIntro + `That was the best discard! You earned 5 akocoins!`;
+                } else if (ranking === 4) {
+                    responseIntro = responseIntro + `That was the number 5 discard! You earned 1 akocoin!`;
                 } else {
                     responseIntro = responseIntro + `That was the number ${ranking + 1} discard! You earned ${5 - ranking} akocoins!`;
                 }
